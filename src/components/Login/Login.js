a0720c530676344b4989ad6bf42dcbe861f8a5c0
git_practice_again <hasanmehedi4484@gmail.com>
2021-11-05 22:22:23 +0600

    useAuth hooks and context api intregrate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import useAuth from '../../hooks/useAuth';
 import './Login.css';
 
 const Login = () => {
-    const {signInUsingGoogle, user} = useFirebase();
+    const {signInUsingGoogle, user} = useAuth();
     return (
         <div className="login-form">
-            <div>
-                <h2>Login</h2>
-                <form action="">
-                    <input type="text" placeholder="Email" /> <br />
-                    <input type="text" placeholder="Password" /> <br />
-                    <input type="submit" value="Submit" />
-                </form>
-                <span>new to ema-john?  </span> <Link to="/register">Create Account</Link>
-                <div>-------or--------</div>
-                <button 
-                    onClick={signInUsingGoogle} 
-                    className="btn-regular"
-                >Google Sign In</button>
-            </div>
+            {
+                !user.email && <div>
+                    <h2>Login</h2>
+                    <form action="">
+                        <input type="text" placeholder="Email" /> <br />
+                        <input type="text" placeholder="Password" /> <br />
+                        <input type="submit" value="Submit" />
+                    </form>
+                    <span>new to ema-john?  </span> <Link to="/register">Create Account</Link>
+                    <div>-------or--------</div>
+                    <button 
+                        onClick={signInUsingGoogle} 
+                        className="btn-regular"
+                    >Google Sign In</button>
+                </div>
+            }
         </div>
     );
 };
